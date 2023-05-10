b7eee047dd0d1c220f86c7ddb8d6671ef28c4385
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-05-10 01:15:11 +0100

    new commits

--- src/Components/HomePage/Header.js
+++ src/Components/HomePage/Header.js
@@ -1,15 +1,48 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const links = (
+    <>
+    <Link to='/' className='navItems'>Home</Link>
+    <Link to='/about-us' className='navItems'>About Us</Link>
+    <Link to='/booking' className='navItems'>Book an Appointment</Link>
+    </>
+  )
+// const screenWidth = window.innerWidth;
+// function Header () {
+//     return (
+//     <>
+//       <header>
+//        <div>
+//        <Link to='/' className='gdave'>Gdave</Link>
+//        {
+//         screenWidth >= 600 ? ({links}) : null
+//        }
+//        </div> 
+//       </header>
+//     </>
+//     )
+// }
+
 
 function Header () {
-    return (
-    <>
-      <header>
-      <p>Gdave</p>
-      <Link to='/' className='navItems'>Home</Link>
-      <Link to='/about-us' className='navItems'>About Us</Link>
-      <Link to='/booking' className='navItems'>Book an Appointment</Link>
-      </header>
-    </>
-    )
+  const [screenWidth, setScreenWidth] = useState(null);
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <header>
+    <div>
+      <Link to='/' className='gdave'>Gdave</Link>
+      {screenWidth && screenWidth >= 600 ? links : null}
+    </div>
+    </header>
+  );
 }
+
 export default Header;
