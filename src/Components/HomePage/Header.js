e912175fad6775d54f6f8eeff5c4c1a65ee8062e
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-05-12 23:01:57 +0100

    .

--- src/Components/HomePage/Header.js
+++ src/Components/HomePage/Header.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const links = (
     <>
@@ -11,18 +11,59 @@ const links = (
  
 function Header () {
   const [screenWidth, setScreenWidth] = useState(null);  
-  useEffect(() => {
-    setScreenWidth(window.innerWidth);
-    const handleResize = () => setScreenWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
+  useEffect(
+     () => {
+       setScreenWidth(window.innerWidth);
+       const handleResize = () => setScreenWidth(window.innerWidth);
+       window.addEventListener("resize", handleResize);
+       return () => window.removeEventListener("resize", handleResize);
+      }, []
+    );
+    const hamburgerRef = useRef(null)
+     useEffect (
+      ()=>{
+        hamburgerRef.current.addEventListener(
+          'click', 
+          ()=>{
+            hamburgerRef.current.classList.toggle('change')
+          }
+        )
+      }
+     )
+  const hamburger = (
+      <div ref={hamburgerRef} className='container' >
+         <div className="bar1"></div>
+         <div className="bar2"></div>
+         <div className="bar3"></div>
+      </div>
+  )
+
+
+
+  const headRef = useRef(null)
+  useEffect (
+    ()=>{
+         function handleScroll () {
+        if (window.scrollY >= 100) {
+           headRef.current.style.backgroundColor  = "#ffffff"
+           headRef.current.style.color  = "#000000"
+        } else {
+            headRef.current.style.backgroundColor = '#00000026';
+       }
+      }
+      window.addEventListener('scroll', handleScroll );
+      return () => {
+      window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
+  
   return (
-    <header>
+    <header ref={headRef}>
     <div>
       <Link to='/' className='gdave'>GDave</Link>
-      {screenWidth && screenWidth >= 600 ? links : null}
+      {screenWidth && screenWidth >= 622 ? links : hamburger}
     </div>
     </header>
   );
