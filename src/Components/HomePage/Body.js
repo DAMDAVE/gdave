8fcf669f4a2c942757636d3476ddc65f844c9cfb
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-05-14 20:45:58 +0100

    .

--- src/Components/HomePage/Body.js
+++ src/Components/HomePage/Body.js
@@ -1,8 +1,10 @@
+import firstDisplayImage from './../../Assets/Images/gdave4.jpg';
+
  function Body () {
     return ( 
     <>
     <div>  
-        <div className="intro">
+        <div className="intro" style={{backgroundImage : require(firstDisplayImage)}}>
             <div className="spacer"></div>
             <h1 className="heading">Welcome to GDave</h1>
             <p>The Body components go in here</p>
--- src/Components/HomePage/Header.js
+++ src/Components/HomePage/Header.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-
 function Header () {
 const links = (
     <>
