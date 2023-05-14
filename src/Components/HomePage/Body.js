3ea56a9d75bd88f7207d9224164f8815ad88cdf2
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-05-14 20:55:34 +0100

    .

--- src/Components/HomePage/Body.js
+++ src/Components/HomePage/Body.js
@@ -1,10 +1,8 @@
-import firstDisplayImage from './../../Assets/Images/gdave4.jpg';
-
  function Body () {
     return ( 
     <>
     <div>  
-        <div className="intro" style={{backgroundImage : require(firstDisplayImage)}}>
+        <div className="intro">
             <div className="spacer"></div>
             <h1 className="heading">Welcome to GDave</h1>
             <p>The Body components go in here</p>
